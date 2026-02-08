f9848c6182f432691cc5b241504f9b45e9517bd5
SergPit <sergpiten@gmail.com>
2026-02-08 22:55:08 +0100

    fix serialization

--- AiSandBox.ApplicationServices/Saver/Persistence/Sandbox/States/MapSquareCellsState.cs
+++ AiSandBox.ApplicationServices/Saver/Persistence/Sandbox/States/MapSquareCellsState.cs
@@ -1,8 +1,13 @@
+using AiSandBox.Infrastructure.Converters;
+using System.Text.Json.Serialization;
+
 namespace AiSandBox.ApplicationServices.Saver.Persistence.Sandbox.States;
 
 public record MapSquareCellsState
 {
     public int Width { get; init; }
     public int Height { get; init; }
+
+    [JsonConverter(typeof(TwoDimensionalArrayConverter<CellState>))]
     public CellState[,] CellGrid { get; init; } = null!;
 }
